8884adb4c48aeb265b9ebfaac1bf6872921e5fea
mqrek <116188885+mqrek@users.noreply.github.com>
2024-11-22 12:52:43 +0100

    added script to filter images regarding our relevant annotations

---
+++ data/Mapillary/mtsd_v2_fully_annotated/relevant_images.py
@@ -0,0 +1,33 @@
+#filters accordingly to the annotations the right images.
+import os
+import json
+import shutil
+
+def filter_images(annotation_dir, image_dir, output_image_dir):
+    os.makedirs(output_image_dir, exist_ok=True)
+    relevant_count = 0
+
+    for annotation_file in os.listdir(annotation_dir):
+        if annotation_file.endswith('.json'):
+            annotation_path = os.path.join(annotation_dir, annotation_file)
+            with open(annotation_path, 'r') as fid:
+                annotation = json.load(fid)
+            
+            image_key = os.path.splitext(annotation_file)[0]
+            image_filename = f"{image_key}.jpg"  
+            
+            image_path = os.path.join(image_dir, image_filename)
+            if os.path.exists(image_path):
+                relevant_count += 1
+
+                shutil.copy(image_path, os.path.join(output_image_dir, image_filename))
+
+    print(f"Gefilterte Bilder: {relevant_count} relevante Bilder gespeichert.")
+
+
+if __name__ == "__main__":
+    annotation_dir = "annotations_relevant"  
+    image_dir = "all_images" 
+    output_image_dir = "images_relevant" 
+
+    filter_images(annotation_dir, image_dir, output_image_dir)
